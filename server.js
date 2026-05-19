b44fb34233cbc614a8261a359a175bdbda5e4e8f
Maximumhonor <maximumhonor@gmail.com>
2026-05-19 11:19:05 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.json({ limit: '10mb' }));
-app.use(express.static('public'));
+app.use(express.static('.'));
 
 const DB_FILE = path.join(__dirname, '.data', 'db.json');
 
